5a3e7afcad449b4931abe0e4b78dd24c868f29df
Ricard_MBP16 <rikitzzz@gmail.com>
2021-03-28 12:39:45 +0200

    chore: reorganized folder structure for streams

--- src/6_streams/writable/to-file-stream.js
+++ src/6_streams/writable/to-file-stream.js
@@ -21,3 +21,18 @@ tfs.write({ path: join('files', 'file1.txt'), content: 'Hello' });
 tfs.write({ path: join('files', 'file2.txt'), content: 'NodeJS' });
 tfs.write({ path: join('files', 'file3.txt'), content: 'streams' });
 tfs.end(() => console.log('All files created!'));
+
+const tfsSimplified = new Writable({
+  objectMode: true,
+  write(chunk, encoding, done) {
+    fs.mkdir(dirname(chunk.path), { recursive: true })
+      .then(() => console.log(`Path ${chunk.path} created!`))
+      .then(() => fs.writeFile(chunk.path, chunk.content))
+      .then(done)
+      .catch(done);
+  }
+});
+tfsSimplified.write({ path: join('files-s', 'file1.txt'), content: 'Hello' });
+tfsSimplified.write({ path: join('files-s', 'file2.txt'), content: 'NodeJS' });
+tfsSimplified.write({ path: join('files-s', 'file3.txt'), content: 'streams' });
+tfsSimplified.end(() => console.log('All files created!'));
